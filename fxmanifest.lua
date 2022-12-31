6085fe5eb0911425884fb46289770017c0d31395
RexShack <rexshackuk@gmail.com>
2022-12-31 06:46:52 +0000

    tidy up

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,21 @@
-fx_version "adamant"
-games {"rdr3"}
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
 
 server_scripts {
-	'server/main.lua'
+    'server/main.lua'
 }
 
 shared_scripts {
-	'@rsg-core/shared/locale.lua',
-	'locales/en.lua',
-	'config.lua'
+    '@rsg-core/shared/locale.lua',
+    'locales/en.lua',
+    'config.lua'
 }
 
 client_scripts {
-	'client/main.lua'
+    'client/main.lua'
 }
 
 dependency 'rsg-core'
+
+lua54 'yes'
